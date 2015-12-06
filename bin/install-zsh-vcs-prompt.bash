8ec325a90ac215de5a8387fec0c3631e6b5faf0e
Yuki Adachi <yuki777@gmail.com>
2015-12-06 23:48:24 +0700

    Change repository from yonchu to yuki777

--- bin/install-zsh-vcs-prompt.bash
+++ bin/install-zsh-vcs-prompt.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-mkdir -p ~/.zsh && cd ~/.zsh && git clone git://github.com/yonchu/zsh-vcs-prompt.git
+#mkdir -p ~/.zsh && cd ~/.zsh && git clone git://github.com/yonchu/zsh-vcs-prompt.git
+mkdir -p ~/.zsh && cd ~/.zsh && git clone git://github.com/yuki777/zsh-vcs-prompt.git
