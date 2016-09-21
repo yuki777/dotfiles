b6a3c962049e49b0c0b2ff4f6978e2320fe83325
Yuki Adachi <yuki777@gmail.com>
2016-09-21 10:31:41 +0700

    Set lua option

--- bin/install-vim.bash
+++ bin/install-vim.bash
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
-set -eu
+#set -eu
+
+# Set lua option
+enable_luainterp="--enable-luainterp=no"
+with_lua_prefix=
+lua_path=`which lua`
+if [ "$?" == "0" ];then
+    if [ "$lua_path" == "/usr/local/bin/lua" ]; then
+        lua_prefix=/usr/local
+    elif [ "$lua_path" == "/usr/bin/lua" ]; then
+        lua_prefix=/usr
+    elif [ "$lua_path" == "$HOME/local/bin/lua" ]; then
+        lua_prefix=$HOME/local
+    else
+        lua_prefix=/usr
+    fi
+    enable_luainterp="--enable-luainterp=yes"
+    with_lua_prefix="--with-lua-prefix=$lua_prefix"
+fi
 
 # install
 cd $HOME
@@ -9,6 +27,8 @@ cd $HOME/local/src
 rm -fr vim
 git clone https://github.com/vim/vim.git
 cd vim/src
-./configure --without-x --disable-gui --enable-largefile --enable-multibyte --disable-selinux --prefix=$HOME/local
+./configure \
+ --without-x --disable-gui --enable-largefile --enable-multibyte --disable-selinux \
+ $enable_luainterp $with_lua_prefix --prefix=$HOME/local
 make
 make install
