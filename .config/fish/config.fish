9a675f19870ec2f6d2812d4288bf17de88439522
Yuki Adachi <yuki-adachi@fabrica-vietnam.com>
2016-09-26 15:07:04 +0700

    Add right_prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,6 +58,9 @@ function fish_prompt
 		echo
 end
 
+function fish_right_prompt
+end
+
 # http://qiita.com/susieyy/items/ac2133e249f252dc9a34
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)' # Bind for peco history to Ctrl+r
