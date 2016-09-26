4efa8c9a4ecf9a0615b6e26407ce99244117c5ae
Yuki Adachi <yuki-adachi@fabrica-vietnam.com>
2016-09-26 12:45:40 +0700

    Use peco v0.4.2

--- bin/install-peco.bash
+++ bin/install-peco.bash
@@ -4,8 +4,8 @@ set -eu
 mkdir -p ~/local/src
 mkdir -p ~/local/bin
 cd ~/local/src
-rm -fr peco_linux_amd64
-wget 'https://github.com/peco/peco/releases/download/v0.3.3/peco_linux_amd64.tar.gz'
+rm -fr peco_linux_amd64*
+wget 'https://github.com/peco/peco/releases/download/v0.4.2/peco_linux_amd64.tar.gz'
 tar zxvf peco_linux_amd64.tar.gz
 cp peco_linux_amd64/peco ~/local/bin/peco
 chmod 744 ~/local/bin/peco
