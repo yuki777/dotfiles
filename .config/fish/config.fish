72d3b09e26b775d9d86b012796632d95433612d3
Yuki Adachi <yuki777@gmail.com>
2016-10-06 19:47:58 +0700

    Add fish variable file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,4 @@ source ~/.config/fish/prompt.fish
 source ~/.config/fish/alias.fish
 source ~/.config/fish/function.fish
 source ~/.config/fish/bind.fish
+source ~/.config/fish/variable.fish
---
+++ .config/fish/variable.fish
@@ -0,0 +1,19 @@
+set PATH $HOME/bin $PATH
+set PATH $HOME/local/bin $PATH
+set PATH /usr/sbin $PATH
+set PATH /sbin $PATH
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/heroku/bin $PATH
+set PATH $HOME/.podenv/bin $PATH
+set PATH $HOME/.rvm/bin $PATH
+
+set LANG en_US.UTF-8
+set LC_CTYPE en_US.UTF-8
+set MANPATH /opt/local/man $MANPATH
+set EDITOR vim
+set SVN_EDITOR vim
+set HGEDITOR vim
+set TERM xterm-256color
+set LD_LIBRARY_PATH $HOME/local/lib/ $LD_LIBRARY_PATH
+set GOROOT ~/local/go
+set NVM_DIR /Users/yuki/.nvm
