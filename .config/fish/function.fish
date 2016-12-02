923b34b4af26a81271ca5c7ff06f8f2746d0534d
Yuki Adachi <yuki-adachi@fabrica-vietnam.com>
2016-12-02 11:42:47 +0700

    Add function hi

--- .config/fish/function.fish
+++ .config/fish/function.fish
@@ -12,16 +12,16 @@ end
 #    vim $(grep -in ${1} **/*.twig | grep -v "[0-9]:\s*//" | peco | awk -F ":" '{print "-c "$2" "$1}')
 #end
 
-## highlighter
-## tail -f log | hi
-## tail -f log | hi hoge
-#function hi
-#    if [ $# != 0 ];then
-#        clide -e /deprecate/i,fg=yellow -e /warn/i,fg=yellow -e /reject/i,fg=red -e /critical/i,fg=red -e /fail/i,fg=red -e /false/i,fg=red -e /err/i,fg=red,bold -e /fatal/i,fg=red,bold -e /\ 404\ /i,bold -e /\ 503\ /i,bold -e /yuki/i,bold -e /adachi/i,fg=blue,bg=yellow -e /$*/i,fg=green
-#    else
-#        clide -e /deprecate/i,fg=yellow -e /warn/i,fg=yellow -e /reject/i,fg=red -e /critical/i,fg=red -e /fail/i,fg=red -e /false/i,fg=red -e /err/i,fg=red,bold -e /fatal/i,fg=red,bold -e /\ 404\ /i,bold -e /\ 503\ /i,bold -e /yuki/i,bold -e /adachi/i,fg=blue,bg=yellow
-#    fi
-#end
+# highlighter
+# tail -f log | hi
+# tail -f log | hi hoge
+function hi
+	if test (count $argv) -ne 0
+		clide -e /deprecate/i,fg=yellow -e /warn/i,fg=yellow -e /reject/i,fg=red -e /critical/i,fg=red -e /fail/i,fg=red -e /false/i,fg=red -e /err/i,fg=red,bold -e /fatal/i,fg=red,bold -e /\ 404\ /i,bold -e /\ 503\ /i,bold -e /yuki/i,bold -e /adachi/i,fg=blue,bg=yellow -e /$argv/i,fg=green
+	else
+		clide -e /deprecate/i,fg=yellow -e /warn/i,fg=yellow -e /reject/i,fg=red -e /critical/i,fg=red -e /fail/i,fg=red -e /false/i,fg=red -e /err/i,fg=red,bold -e /fatal/i,fg=red,bold -e /\ 404\ /i,bold -e /\ 503\ /i,bold -e /yuki/i,bold -e /adachi/i,fg=blue,bg=yellow
+	end
+end
 
 # https://github.com/yoshiori/fish-peco_select_zsh_history
 function peco_select_zsh_history
