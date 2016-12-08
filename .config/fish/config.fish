2e3d35ce28a725e72202d92463e5515cb8e352fe
Yuki Adachi <yuki777@gmail.com>
2016-12-09 01:09:27 +0700

    history --merge

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,9 @@ source ~/.config/fish/alias.fish
 source ~/.config/fish/function.fish
 source ~/.config/fish/bind.fish
 source ~/.config/fish/variable.fish
+
+# https://github.com/fish-shell/fish-shell/issues/825
+function sync_history --on-event fish_preexec
+  history --save
+  history --merge
+end
