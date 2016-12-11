d7fbf5fb31b7844c4c5a51f6a6814f969e92f049
Yuki Adachi <yuki777@gmail.com>
2016-12-11 07:57:28 +0700

    Refactor

--- bin/setup-vim.bash
+++ bin/setup-vim.bash
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-## setup neobundle, plugins
-#cd $HOME
-#mkdir -p $HOME/.vim/bundle
-#git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
-#git clone git://github.com/Shougo/unite.vim     $HOME/.vim/bundle/unite.vim
-#git clone git://github.com/Shougo/vimproc.vim   $HOME/.vim/bundle/vimproc
-#if [ "`uname`" = "Darwin" ];then
-#    cd $HOME/.vim/bundle/vimproc && make -f make_mac.mak
-#elif [ "`uname`" = "FreeBSD" ];then
-#    cd $HOME/.vim/bundle/vimproc && make -f make_bsd.mak
-#else
-#    cd $HOME/.vim/bundle/vimproc && make -f make_unix.mak
-#fi
-#$HOME/.vim/bundle/neobundle.vim/bin/neoinstall
-
 # yankring,yanktmp
 mkdir -p ~/.vim/data/yankring
 mkdir -p ~/.vim/data/yanktmp
@@ -23,3 +8,5 @@ mkdir -p ~/.vim/data/yanktmp
 # neosnippet
 mkdir -p ~/.vim/data/neosnippet
 cp ~/dotfiles/.vim/data/neosnippet/* ~/.vim/data/neosnippet/
+
+vim -c 'qa!'
