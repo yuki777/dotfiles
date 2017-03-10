4c21f1eee8c45070638333806638b737c99857a2
Yuki Adachi <yuki-adachi@fabrica-vietnam.com>
2017-03-10 09:04:45 +0700

    Unsupport FreeBSD

--- bin/install-peco.bash
+++ bin/install-peco.bash
@@ -10,18 +10,15 @@ if [ -f ~/local/bin/peco ];then
 fi
 
 case "${OSTYPE}" in
+    freebsd*)
+        echo "Sorry, I can't support FreeBSD."
+        exit 0
+        ;;
     darwin*)
         rm -fr peco_*
         wget 'https://github.com/peco/peco/releases/download/v0.5.0/peco_darwin_amd64.zip'
         unzip peco_darwin_amd64.zip
         cp peco_darwin_amd64/peco ~/local/bin/peco
-        chmod 744 ~/local/bin/peco
-        ;;
-    freebsd*)
-        rm -fr peco_*
-        wget 'https://github.com/peco/peco/releases/download/v0.5.0/peco_linux_amd64.tar.gz' # or 0.4.2 ?
-        tar zxvf peco_linux_amd64.tar.gz
-        cp peco_linux_amd64/peco ~/local/bin/peco
         chmod 744 ~/local/bin/peco
         ;;
     linux*)
