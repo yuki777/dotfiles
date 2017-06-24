559f6d781cfd1b879a4ae3d12154b23f304df2b2
Yuki Adachi <yuki777@gmail.com>
2017-06-25 01:23:20 +0700

    Install tmux 2.5

--- bin/install-tmux.bash
+++ bin/install-tmux.bash
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
+version=2.5
+
 mkdir -p ~/local/src
 mkdir -p ~/local/bin
 cd ~/local/src
-rm -fr tmux-2.0
-wget 'https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz'
-tar zxvf tmux-2.0.tar.gz
-cd tmux-2.0
+rm -fr tmux-$version
+wget "https://github.com/tmux/tmux/releases/download/$version/tmux-$version.tar.gz"
+tar zxvf tmux-$version.tar.gz
+cd tmux-$version
 ./configure --prefix=$HOME/local
-make
-make install
+make && make install
