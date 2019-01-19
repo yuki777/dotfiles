3c47ea0ae001428272451c6e947484da6bf1d2ac
Yuki Adachi <yuki777@gmail.com>
2019-01-19 20:19:17 +0900

    Update version to 0.5.3

--- bin/install-peco.bash
+++ bin/install-peco.bash
@@ -16,14 +16,14 @@ case "${OSTYPE}" in
         ;;
     darwin*)
         rm -fr peco_*
-        wget 'https://github.com/peco/peco/releases/download/v0.5.0/peco_darwin_amd64.zip'
+        wget 'https://github.com/peco/peco/releases/download/v0.5.3/peco_darwin_amd64.zip'
         unzip peco_darwin_amd64.zip
         cp peco_darwin_amd64/peco ~/local/bin/peco
         chmod 744 ~/local/bin/peco
         ;;
     linux*)
         rm -fr peco_*
-        wget 'https://github.com/peco/peco/releases/download/v0.5.0/peco_linux_amd64.tar.gz'
+        wget 'https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz'
         tar zxvf peco_linux_amd64.tar.gz
         cp peco_linux_amd64/peco ~/local/bin/peco
         chmod 744 ~/local/bin/peco
