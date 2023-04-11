387d8ec8102caadf283e93a4b5d03ca4722aa232
Yuki Adachi <brand_adachi@shueisha.co.jp>
2023-04-11 21:07:08 +0900

    Update

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,10 +1,24 @@
--- 開始時にカレントディレクトリのツリーを表示する
-vim.cmd("autocmd VimEnter * Neotree toggle")
-
--- 開始時に前回終了時のセッションを開く
-vim.cmd("autocmd VimEnter * SessionManager load_current_dir_session")
-
--- セッションを終了時に自動保存
-vim.cmd("autocmd VimLeave * SessionManager save_current_session")
-
-return {}
+---- 開始時にカレントディレクトリのツリーを表示する
+----vim.cmd("autocmd VimEnter * Neotree toggle")
+--vim.cmd("autocmd VimEnter * Neotree")
+--
+---- 開始時に前回終了時のセッションを開く
+--vim.cmd("autocmd VimEnter * SessionManager load_current_dir_session")
+--
+---- セッションを終了時に自動保存
+--vim.cmd("autocmd VimLeave * SessionManager save_current_session")
+--
+--return {}
+return
+{
+	-- https://www.lazyvim.org/plugins/ui#bufferlinenvim
+	"https://github.com/akinsho/bufferline.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function() require('bufferline').setup({
+		options = {
+			mode = "tabs",
+		},
+	}) end
+}
--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -1,2 +1,43 @@
--- <F12> Neotreeと行表示を非表示にする
-vim.api.nvim_set_keymap('n', '<F12>', ':Neotree close<CR>:set number! relativenumber!<CR>', {noremap = true, silent = true})
+-- -- <F1>  ファイルのFullpathを表示しつつ、Fullpathをコピーする
+-- vim.api.nvim_set_keymap('n', '<F1>', ':let @*=expand("%:p")<CR>:echo expand("%:p")<CR>', {noremap = true, silent = true})
+-- -- <F2>  設定可能
+-- vim.api.nvim_set_keymap('n', '<F2>', ':set paste!<CR>', {noremap = true, silent = true})
+-- -- <F3>  設定可能
+-- vim.api.nvim_set_keymap('n', '<F3>', ':echo 3<CR>', {noremap = true})
+-- -- <F4>  設定可能
+-- vim.api.nvim_set_keymap('n', '<F4>', ':echo 4<CR>', {noremap = true})
+-- -- <F5>  デバッグ実行する
+-- -- <F6>  ?
+-- -- <F7>  ターミナルを開く
+-- -- <F8>  設定可能
+-- vim.api.nvim_set_keymap('n', '<F8>', ':echo 8<CR>', {noremap = true})
+-- -- <F9>  デバッグブレイクポイントを設定する
+-- -- <F10> ?
+-- -- <F11> ?
+-- -- <F12> Neotreeを閉じて、Gitsignsをトグルして、行表示をトグルする
+-- vim.api.nvim_set_keymap('n', '<F12>', ':Neotree close<CR>:Gitsigns toggle_signs<CR>:set number! relativenumber!<CR>', {noremap = true, silent = true})
+-- 
+-- -- <tab> nextバッファへ移動する
+-- vim.api.nvim_set_keymap('n', '<tab>', ':bnext<CR>', {noremap = true, silent = true})
+-- -- <S-tab> previousバッファへ移動する
+-- vim.api.nvim_set_keymap('n', '<S-tab>', ':bprevious<CR>', {noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Down>', 'gj', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Up>', 'gk', { noremap = true })
+vim.api.nvim_set_keymap('n', 'gj', 'j', { noremap = true })
+vim.api.nvim_set_keymap('n', 'gk', 'k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W>l', { noremap = true })
+vim.api.nvim_set_keymap('n', '<S-h>', '^', { noremap = true })
+vim.api.nvim_set_keymap('n', '*', '*N', { noremap = true })
+vim.api.nvim_set_keymap('n', '#', '#N', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohl<CR>:set nopaste<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<tab>', ':tabnext<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-tab>', ':tabprevious<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-d>', ':Neotree toggle<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F11>', ':set paste<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F12>', ':Neotree close<CR>:Gitsigns toggle_signs<CR>:set number! relativenumber!<CR>', {noremap = true, silent = true})
